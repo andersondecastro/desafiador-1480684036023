afb854adc568f49681e83d684c034860173636f2
Anderson de Castro <anderson.in.net@gmail.com>
2016-12-02 11:08:37 -0300

    Alteração de nome de usuario sem espaços

--- proccess.php
+++ proccess.php
@@ -6,7 +6,7 @@
 	$senha = $_POST['senha'];
 
 	// gerando usuario
-	$usuario = strtolower( $nome ) . '-' . uniqid();
+	$usuario = trim( strtolower( $nome ) ) . '-' . uniqid();
 
 
 	$db = new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;port=3306;dbname=ad_e5455148597d6e5;charset=UTF8;','b8ce7835c99f85','110552ba');
