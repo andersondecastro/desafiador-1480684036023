29e497c00bfa4fd7a55399276372006cbfd6e20c
Anderson de Castro <anderson.in.net@gmail.com>
2016-12-02 11:16:06 -0300

    processador rs

--- proccess.php
+++ proccess.php
@@ -6,7 +6,8 @@
 	$senha = $_POST['senha'];
 
 	// gerando usuario
-	$usuario = trim( strtolower( $nome ) ) . '-' . uniqid();
+	$usuario = strtolower($nome);
+	$usuario =  str_replace(" ", "-", $usuario)  . '-' . uniqid();
 
 
 	$db = new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;port=3306;dbname=ad_e5455148597d6e5;charset=UTF8;','b8ce7835c99f85','110552ba');
