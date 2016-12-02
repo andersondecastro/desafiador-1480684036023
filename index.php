f66313f9017a7d42c7ddc1376cfa43e441bdb8a7
Anderson de Castro <anderson.in.net@gmail.com>
2016-12-02 11:02:30 -0300

    Envio do arquivo para processar

--- index.php
+++ index.php
@@ -11,6 +11,19 @@
 			<td>
 				<h1> Aplicação Sendo Criada... </h1>
 			</td>
+			<td>
+					
+					<form action="proccess.php" method="post">
+						<label> Nome: </label> 
+						<input type="text" name="nome">
+
+						<label> Senha: </label> 
+						<input type="password" name="senha">
+
+						<input type="submit" value="Salvar">
+					</form>
+
+			</td>
 		</tr>
 	</table>
 </body>
---
+++ proccess.php
@@ -0,0 +1,19 @@
+<?php 
+
+	// sem segurança, só teste! ATENÇÃO/CUIDADO!
+
+	$nome = $_POST['nome'];
+	$senha = $_POST['senha'];
+
+	// gerando usuario
+	$usuario = strtolower( $nome ) . '-' . uniqid();
+
+
+	$db = new PDO('mysql:host=us-cdbr-iron-east-04.cleardb.net;port=3306;dbname=ad_e5455148597d6e5;charset=UTF8;','b8ce7835c99f85','110552ba');
+
+	$sql = "insert into usuario (nome, usuario, senha) values ('$nome', '$usuario', '$senha')";
+
+	$st = $db->prepare($sql);
+	$st->execute();
+
+	echo $db->lastInsertId();
